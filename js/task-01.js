22a63788877b9e1c96c03b98b4f4fd54871159eb
Anton890413 <antonzaycev1989@gmail.com>
2020-12-04 11:49:52 +0200

    alldone

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,13 @@
 const allCategories = document.querySelectorAll(".item");
 console.log(`В списке ${allCategories.length} категории.`);
 
-const categoryName = [...allCategories]
+/*const categoryName = [...allCategories]
   .map(
     categories => `Категория: ${categories.children[0].textContent}
 Количество элементов: ${categories.children[1].children.length}`
   )
   .join("\n");
-console.log(categoryName);
+console.log(categoryName);*/
+
+allCategories.forEach(categories => console.log(`Категория: ${categories.children[0].textContent}
+Количество элементов: ${categories.children[1].children.length}`));
--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,24 @@
 const input = document.querySelector('#validation-input');
 
+const validValue = Number(input.dataset.length);
+
 function validation() {
-    if (input.value.length < input.dataset.length) {
-        input.classList.remove('valid');
-        input.classList.add('invalid');
-    } else {
-        input.classList.remove('invalid');
-        input.classList.add('valid');
-    }
-}
+
+  if (input.value.length === validValue) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else if (input.value.length === 0) {
+    input.classList.remove('invalid');
+    input.classList.remove('valid');
+  } else {
+    input.classList.add('invalid');
+    input.classList.remove('valid');
+  }
+};
+
 input.addEventListener('blur', validation);
+
+
+
+
 
